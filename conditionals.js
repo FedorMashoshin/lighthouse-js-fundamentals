f36d472184cc9e9400f690f01393cf1d22ba9584
Fedor_Mashoshin <fmashoshin@gmail.com>
2020-03-10 16:52:05 +1000

    Little practice with conditionals

---
+++ conditionals.js
@@ -0,0 +1,9 @@
+const temp = 15;
+ 
+if (temp < 0 ) {
+    console.log('The weather is cold. Sit at home:-0');
+} else if (temp >= 0 && temp < 15){
+    console.log('The weather is fine. Dont forget about your coat!')
+} else {
+    console.log('What a weather! Let\'s go on the beach xD ')
+}
