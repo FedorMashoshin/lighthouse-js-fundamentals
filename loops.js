afe4aea7775262518035b8dfeb65ee5a3cdcf614
Fedor_Mashoshin <fmashoshin@gmail.com>
2020-03-11 15:01:25 +1000

    Created file loops.js

---
+++ loops.js
@@ -0,0 +1,15 @@
+//!============== WHILE ==============\\
+const chorus = "Let's dance!";
+let repeat = 0;
+while (repeat < 5) {
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
+
+
+//!============== FOR ==============\\
+for (i = 0; i < 5; i ++){
+    console.log('Yes!')
+} 
+console.log('No!')
